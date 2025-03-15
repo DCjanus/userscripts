90463fb53f2a05ee9e83d6d262acdf1145842f89
DCjanus <DCjanus@dcjanus.com>
2025-03-15 15:43:31 +0800

    fix(MoviePlus): update search URL for 独播库

--- scripts/MoviePlus.user.js
+++ scripts/MoviePlus.user.js
@@ -76,7 +76,7 @@ function update_bt_site(title, year, douban_ID, IMDb_ID, title_cn) {
             'https://www.btdig.com/search?q=' + title + ' ' + year + ' 1080p',
         'BTDigg 中': 'https://www.btdig.com/search?q=' + title_cn,
         'EXT.TO': 'https://ext.to/search/?q=' + title,
-        缘觉影视: 'https://www.yjys05.com/search/' + title_cn,
+        独播库: 'https://www.duboku.tv/vodsearch/-------------.html?wd=' + title_cn + '&submit=',
         动漫花园: 'https://dmhy.org/topics/list?keyword=' + title,
     };
 
